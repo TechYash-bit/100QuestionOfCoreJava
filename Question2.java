63d56157a93a4cf934fc9f19bfc6dc6c43219fb7
TechYash-bit <199874962+TechYash-bit@users.noreply.github.com>
2025-06-03 12:06:26 +0530

    day1 Question2

---
+++ Question2.java
@@ -0,0 +1,15 @@
+//Take two number print sum of it
+import java.util.*;
+public class Question2{
+    public static void main(String args[]){
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the first number");
+
+        a=sc.nextInt();
+        System.out.println("enter the second number");
+        b=sc.nextInt();
+
+        System.out.println("sum is  "+ (a+b));
+    }
+}
